676d290f785b7a8c35550891a94fff54ccfb1797
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-19 18:13:35 +0700

    change placeholder data

--- src/app/user.data.ts
+++ src/app/user.data.ts
@@ -5,22 +5,23 @@ export const userData: Array<DataUser> = [
     name: 'Fariz',
     email: 'fariz@mail.com',
     paymentDeadline: new Date(2024, 8, 15),
-    address: {
-      city: 'Tangerang',
-      province: 'Banten',
-      zipcode: 10205,
-    },
+    city: 'Tangerang',
+    province: 'Banten',
+    zipcode: 10205,
     isChecked: false,
+    age: 25,
+    basicSalary: '25000',
   },
   {
     name: 'Rizky',
     email: 'Rizky@mail.com',
     paymentDeadline: new Date(2024, 8, 20),
-    address: {
-      city: 'Tangerang Selatan',
-      province: 'Banten',
-      zipcode: 10310,
-    },
+    city: 'Tangerang Selatan',
+    province: 'Banten',
+    zipcode: 10310,
+
     isChecked: false,
+    age: 25,
+    basicSalary: '25000',
   },
 ];
