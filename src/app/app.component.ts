c27081ce69b995b41850449216fd8e514dfae9ed
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-19 18:06:10 +0700

    cleans app component and use routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,11 @@
-import { Component, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataUser } from './models/app.model';
-import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './button/button.component';
-import { GenerateRandomIdService } from './shared/generate-random-id.service';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { FormComponent } from './form/form.component';
-import { UserTableComponent } from './user-table/user-table.component';
-import { UserdataService } from './shared/userdata.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    ButtonComponent,
-    FormsModule,
-    ReactiveFormsModule,
-    FormComponent,
-    UserTableComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit, OnChanges {
-  title = 'angular-fif';
-  labelButton!: string;
-  userData!: Array<DataUser>;
-  name: string = '';
-  addUserForm!: FormGroup;
-
-  constructor(private userService: UserdataService) {
-    this.userData = userService.getUsers();
-  }
-
-  ngOnInit(): void {
-    
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('asu')
-    console.log(changes)
-  }
-
-  receiveFromChild(event: string): void {
-    this.title = event;
-    this.labelButton = event;
-  }
-
-  checkOnChange(): void {
-    // console.log(this.addUserForm.get('name')?.errors);
-  }
-
-  onSubmit() {
-    console.log('halo from form');
-    console.log(this.addUserForm.get('phoneNumber')?.errors)
-  }
-
-  receiveUser(event: DataUser) {
-    this.userService.addNewUser(event)
-  } 
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './pages/form/form.component';
+import { UserTableComponent } from './pages/user-table/user-table.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: UserTableComponent},
+    {path: 'add', component: FormComponent},
+    {path: 'edit/:id', component: FormComponent},
+    {path: 'not-found', component: NotFoundComponent},
+    {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+];
