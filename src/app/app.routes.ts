33f785a7bb690221627bdcb02e2cc03f2f7e5961
Kevin Ezra <tkevinrocket@gmail.com>
2024-09-20 12:58:17 +0700

    protect route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './pages/form/form.component';
 import { UserTableComponent } from './pages/user-table/user-table.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { authGuard } from '../guard/auth.guard';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: UserTableComponent},
-    {path: 'add', component: FormComponent},
-    {path: 'edit/:id', component: FormComponent},
-    {path: 'not-found', component: NotFoundComponent},
-    {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+  { path: '', component: UserTableComponent, canActivate: [authGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'add', component: FormComponent, canActivate: [authGuard] },
+  { path: 'edit/:id', component: FormComponent, canActivate: [authGuard] },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
